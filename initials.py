3e03d7d14c4d353e9a25726a55b73d3ca7572d9d
spacew0man <40811076+spacew0man@users.noreply.github.com>
2022-07-31 17:10:39 -0400

    Create initials.py
    'Block Letters' project

---
+++ initials.py
@@ -0,0 +1,20 @@
+#Katie Humphreys
+#Fun Fact: I hung out at a VIP afterparty with my favorite band, Taking Back Sunday, in middle school
+
+print("K   K" + "   " + "H   H")
+print("K  K" + "    " + "H   H")
+print("K K" + "     " + "H   H")
+print("KK" + "      " + "HHHHH")
+print("K K" + "     " + "H   H")
+print("K  K" + "    " + "H   H")
+print("K   K" + "   " + "H   H")
+
+print("""
+H   H   EEEEE   Y   Y      N   N   EEEEE   RRRR    DDDD     SSS
+H   H   E        Y Y       NN  N   E       R   R   D   D   S   S
+H   H   E         Y        N N N   E       R   R   D   D   S
+HHHHH   EEE       Y        N  NN   EEE     RRRR    D   D    SSS
+H   H   E         Y        N   N   E       R R     D   D       S
+H   H   E         Y        N   N   E       R  R    D   D   S   S
+H   H   EEEEE     Y        N   N   EEEEE   R   R   DDDD     SSS
+""")
